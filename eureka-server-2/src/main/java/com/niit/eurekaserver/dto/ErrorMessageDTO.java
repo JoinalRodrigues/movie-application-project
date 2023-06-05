49f48285ac0ef0dc3f324537fc9bfc7fc358fbb2
Joinal Rodrigues <joinalrodrigues@gmail.com>
2023-06-05 14:32:48 +0530

    Rectifying eureka-server-2

--- eureka-server-2/src/main/java/com/niit/eurekaserver/dto/ErrorMessageDTO.java
+++ eureka-server-2/src/main/java/com/niit/eurekaserver/dto/ErrorMessageDTO.java
@@ -1,6 +1,7 @@
 package com.niit.eurekaserver.dto;
 
 
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ErrorMessageDTO {
     private String error;
