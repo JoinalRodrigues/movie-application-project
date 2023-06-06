81bcee3569b2b47f64fb3a9df6ac63e3eb28acc1
Joinal Rodrigues <joinalrodrigues@gmail.com>
2023-06-06 20:51:17 +0530

    spring session testing

--- api-gateway/src/main/java/com/niit/apigateway/session/SessionConfiguration.java
+++ api-gateway/src/main/java/com/niit/apigateway/session/SessionConfiguration.java
@@ -2,7 +2,9 @@ package com.niit.apigateway.session;
 
 
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableRedisWebSession
 public class SessionConfiguration {
 
