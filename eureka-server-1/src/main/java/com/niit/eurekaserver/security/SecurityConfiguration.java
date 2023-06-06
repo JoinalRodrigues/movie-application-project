c4fb151d3eefd757bdbd4603bfc937518beb2905
Joinal Rodrigues <joinalrodrigues@gmail.com>
2023-06-06 12:19:02 +0530

    spring session testing

--- eureka-server-1/src/main/java/com/niit/eurekaserver/security/SecurityConfiguration.java
+++ eureka-server-1/src/main/java/com/niit/eurekaserver/security/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
-        http.securityMatchers(i -> i.requestMatchers("/eureka", "/eureka**", "/eureka/**", "/api/v1/admin", "/api/v1/admin**", "/api/v1/admin/**", "", "/"))
+        http.securityMatchers(i -> i.requestMatchers("/eureka", "/eureka**", "/eureka/**"))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .csrf()
                 .disable()
@@ -29,17 +29,17 @@ public class SecurityConfiguration {
                 .disable()
                 .httpBasic()
                 .disable()
-                .headers()
-                .frameOptions()
-                .disable()
-                .and()
-                .headers()
-                .contentTypeOptions()
-                .disable()
-                .and()
-                .headers()
-                .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", "frame-ancestors 'self' http://34.83.1.21 http://localhost:4200"))
-                .and()
+//                .headers()
+//                .frameOptions()
+//                .disable()
+//                .and()
+//                .headers()
+//                .contentTypeOptions()
+//                .disable()
+//                .and()
+//                .headers()
+//                .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy", "frame-ancestors 'self' http://34.83.1.21 http://localhost:4200"))
+//                .and()
                 .authenticationProvider(new AuthenticationManagerBeanDefinitionParser.NullAuthenticationProvider())
                 .authorizeHttpRequests(i -> i.anyRequest().permitAll());
         return http.build();
@@ -48,8 +48,8 @@ public class SecurityConfiguration {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE + 1)
     public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
-        http.securityMatcher("/", "", "/**")
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
+        http.securityMatcher("/", "", "/**",  "/api/v1/admin", "/api/v1/admin**", "/api/v1/admin/**")
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
                 .csrf()
                 .disable()
                 .cors()
