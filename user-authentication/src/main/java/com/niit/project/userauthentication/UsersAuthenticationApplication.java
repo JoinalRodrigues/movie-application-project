762369f27726fa04de94f6f0996eac77e75beaa4
Joinal Rodrigues <joinalrodrigues@gmail.com>
2023-06-07 14:15:14 +0530

    spring session testing

--- user-authentication/src/main/java/com/niit/project/userauthentication/UsersAuthenticationApplication.java
+++ user-authentication/src/main/java/com/niit/project/userauthentication/UsersAuthenticationApplication.java
@@ -2,11 +2,11 @@ package com.niit.project.userauthentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class UsersAuthenticationApplication {
 
 	public static void main(String[] args) {
